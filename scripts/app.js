4b6ecd2e9281f33f4eef6f562657c4247e10a348
Skoth <kdjohn91@gmail.com>
2015-08-26 18:48:09 -0700

    dropped codemirror and switched to ace editor

--- scripts/app.js
+++ scripts/app.js
@@ -21,10 +21,10 @@
  * SOFTWARE.
  */
 
-(function() {
-	var app = angular.module('Vulcan', ['ngRoute', 'ngSanitize', 'textAngular', 'ui.codemirror']);
+(function () {
+	var app = angular.module('Vulcan', ['ngRoute', 'ngSanitize', 'textAngular']);
 	// test new system
-	app.config(function($routeProvider, $locationProvider) {
+	app.config(function ($routeProvider, $locationProvider) {
 		// Removes hash from routing (?)
 		// $locationProvider.html5Mode(true);
 		
@@ -35,7 +35,7 @@
 			})
 			.when('/quiz', {
 				templateUrl: 'components/QNAs.html',
-				controller:'mainController',
+				controller: 'mainController',
 				activeTab: 'quiz'
 			})
 			.when('/add-qna', {
@@ -49,149 +49,211 @@
 	});
 	
 	// Math Toolbar Button
-	app.config(function($provide) {
-		$provide.decorator('taOptions', ['taRegisterTool', '$delegate', function(taRegisterTool, taOptions) {
+	app.config(function ($provide) {
+		$provide.decorator('taOptions', ['taRegisterTool', '$delegate', function (taRegisterTool, taOptions) {
 			// Code entry toolbar button
-			taRegisterTool('colourRed', {
-				iconclass:'fa fa-terminal',
-				action: function() {
-					this.$editor().wrapSelection('forecolor', 'red');
+			taRegisterTool('Ace', {
+				iconclass: 'fa fa-terminal',
+				action: function () {
+					var self = this;
+					if ($('#codeModal').length === 0) {
+						$('body').append('' +
+							'<div id="codeModal" class="modal fade">' +
+							'<div class="modal-dialog">' +
+							'<div class="modal-content">' +
+							'<div class="modal-header">' +
+							'<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>' +
+							'<h4 class="modal-title text-center">Code Preview</h4>' +
+							'</div>' +
+							'<div class="modal-body">' +
+							'<div id="codeEditor" style="width:100%; height: 350px;"></div>' +
+							'</div>' +
+							'<div class="modal-footer">' +
+							'<button id="cancelCodeBtn" type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>' +
+							'<button id="insertCodeBtn" type="button" class="btn btn-primary" data-dismiss="modal">Insert</button>' +
+							'</div>' +
+							'</div>' +
+							'</div>' +
+							'</div>' +
+							'');
+						$('#codeModal').modal({
+							show: false,
+							keyboard: true
+						});
+
+
+						$('#insertCodeBtn').on('click', function () {
+							window.selectTest = self.$editor();
+						});
+					}
+					$('#codeModal').modal('show');
+
+					var siHash = null;
+
+					$('#codeModal').on('shown.bs.modal', function () {
+						if (typeof ace !== 'undefined') {
+							window.editor = ace.edit("codeEditor");
+							window.editor.setTheme("ace/theme/monokai");
+							window.editor.getSession().setMode("ace/mode/csharp");
+							
+							// Hideous hack to negate blur bug after first character entry on
+							// ace (also mandates focus on ace--as opposed to textAngular--when 
+							// the modal is displayed)
+							siHash = setInterval(function() {
+								console.log('focus');
+								$('#codeEditor textarea').focus();
+							}, 200);
+						}
+						else {
+							console.log('Error loading ace');
+						}
+						console.log('Code modal displayed.');
+						// $('#codeEditor').focus();
+					});
+					
+					$('#codeModal').on('hide.bs.modal', function() {
+						clearInterval(siHash);
+					});
 				}
 			});
-			taOptions.toolbar[2].push('colourRed');
+			taOptions.toolbar[2].push('Ace');
 			
 			// Math entry toolbar button
-			taRegisterTool('colourAzure', {
-				iconclass:'fa fa-superscript',
-				action: function() {
+			taRegisterTool('MathJax', {
+				iconclass: 'fa fa-superscript',
+				action: function () {
+					window.self = self;
 					var self = this;
-					if($('.modal').length === 0) {
+					if ($('#mathModal').length === 0) {
 						$('body').append('' +
-							'<div id="mathModal" class="modal fade">' + 
-								'<div class="modal-dialog">' + 
-									'<div class="modal-content">' + 
-										'<div class="modal-header">' + 
-											'<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>' + 
-											'<h4 class="modal-title text-center">Equation Preview</h4>' + 
-										'</div>' + 
-										'<div class="modal-body">' +
-											'<p><div id="renderedEqn"></div></p>' + 
-											'<p><input id="eqnTextInput" type="text" class="form-control" /></p>' + 
-										'</div>' + 
-										'<div class="modal-footer">' + 
-											'<input id="#inlineEqn" type="checkbox" /> Inline Equation ' +
-											'<button id="insertEqnBtn" type="button" class="btn btn-primary" data-dismiss="modal">Insert</button>' + 
-										'</div>' +
-									'</div>' +
-								'</div>' +
+							'<div id="mathModal" class="modal fade">' +
+							'<div class="modal-dialog">' +
+							'<div class="modal-content">' +
+							'<div class="modal-header">' +
+							'<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>' +
+							'<h4 class="modal-title text-center">Equation Preview</h4>' +
 							'</div>' +
-						'');	
+							'<div class="modal-body">' +
+							'<p><div id="renderedEqn"></div></p>' +
+							'<p><input id="eqnTextInput" type="text" class="form-control" /></p>' +
+							'</div>' +
+							'<div class="modal-footer">' +
+							'<div class="pull-left"><input id="#inlineEqn" type="checkbox" /> Inline Equation </div>' +
+							'<button id="cancelCodeBtn" type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>' +
+							'<button id="insertEqnBtn" type="button" class="btn btn-primary" data-dismiss="modal">Insert</button>' +
+							'</div>' +
+							'</div>' +
+							'</div>' +
+							'</div>' +
+							'');
 						$('#mathModal').modal({
-							show:false,
-							keyboard:true
+							show: false,
+							keyboard: true
 						});
-						
-						
-						$('#insertEqnBtn').on('click', function() {
+
+
+						$('#insertEqnBtn').on('click', function () {
 							window.selectTest = self.$editor();
 							// TODO: use rangy/selection API to insert math content back in text-angular
 							console.log('Insert math content into editor');
 						});
-						
-						$('#eqnTextInput').on('keyup', function() {
+
+						$('#eqnTextInput').on('keyup', function () {
 							$('#renderedEqn').text('$$' + $(this).val().trim() + '$$');
 							MathJax.Hub.Typeset();
 						});
 					}
+					self.$editor.focussed = false;
 					$('#mathModal').modal('show');
-					
-					$('#mathModal').on('shown.bs.modal', function() {
+					$('*:focus').blur();
+
+					$('#mathModal').on('shown.bs.modal', function () {
 						console.log('Math modal displayed.');
 					})
-					
-					self.$editor().wrapSelection('forecolor', 'azure');
+
+					// self.$editor().wrapSelection('forecolor', 'azure');
 				}
 			});
-			taOptions.toolbar[2].push('colourAzure');
-			
+			taOptions.toolbar[2].push('MathJax');
+
 			return taOptions;
 		}]);
 	});
 	
 	// IndexedDB Factory Service
-	app.factory('qnasDBFactory', ['$q', function($q) {
+	app.factory('qnasDBFactory', ['$q', function ($q) {
 		var QNASDB = null,
 			self = this;
 
-		self.OpenDB = function() {
+		self.OpenDB = function () {
 			var deferred = $q.defer(),
 				version = 1;
 			var openRequest = indexedDB.open('QNASDB_' + version, version);
-			
-			openRequest.onupgradeneeded = function(e) {
+
+			openRequest.onupgradeneeded = function (e) {
 				QNASDB = e.target.result;
 				e.target.transaction.onerror = indexedDB.onerror;
 				
 				// TODO: backup for data existing before upgrade
 				// (instead of completely deleting)??
-				if(QNASDB.objectStoreNames.contains('qnas')) {
+				if (QNASDB.objectStoreNames.contains('qnas')) {
 					QNASDB.deleteObjectStore('qnas');
 				}
-				if(QNASDB.objectStoreNames.contains('topics')) {
+				if (QNASDB.objectStoreNames.contains('topics')) {
 					QNASDB.deleteObjectStore('topics');
 				}
-				
+
 				QNASDB.createObjectStore('qnas', { autoIncrement: true });
 				QNASDB.createObjectStore('topics', { autoIncrement: true });
 			};
-			
-			openRequest.onsuccess = function(e) {
+
+			openRequest.onsuccess = function (e) {
 				QNASDB = e.target.result;
 				deferred.resolve(e);
 			}
-			
-			openRequest.onerror = function() {
+
+			openRequest.onerror = function () {
 				deferred.reject(e);
 			}
-			
+
 			return deferred.promise;
 		};
-		
-		self.SelectAll = function() {
+
+		self.SelectAll = function () {
 			var deferred = $q.defer();
-			
-			if(QNASDB === null) deferred.reject('Error: indexedDB unavailable.');
+
+			if (QNASDB === null) deferred.reject('Error: indexedDB unavailable.');
 			else {
 				// test if qnas & topics combined in array yields results of both
 				var transaction = QNASDB.transaction(['qnas', 'topics'], 'readonly');
 				var qnasStore = transaction.objectStore('qnas');
 				var topicsStore = transaction.objectStore('topics');
 				var netData = { qnas: [], topics: {} };
-				
+
 				var keyRange = IDBKeyRange.lowerBound(0);
 				// IDBObjectStore.openCursor() returns IDBRequest object
 				var qnasRequest = qnasStore.openCursor(keyRange);
 				var topicsRequest = topicsStore.openCursor(keyRange);
-				
-				qnasRequest.onsuccess = function(e) {
+
+				qnasRequest.onsuccess = function (e) {
 					var qnasCursor = e.target.result;
-					if(typeof qnasCursor === 'undefined' || qnasCursor === null)
+					if (typeof qnasCursor === 'undefined' || qnasCursor === null)
 						console.log('qnasCursor conditional'); 
-						//deferred.resolve(topics, qnas);
+					//deferred.resolve(topics, qnas);
 					else {
-						netData.qnas.push({ 'key':qnasCursor.key, 'data':qnasCursor.value });
+						netData.qnas.push({ 'key': qnasCursor.key, 'data': qnasCursor.value });
 						qnasCursor.continue();
 					}
 				};
-				
-				qnasRequest.onerror = function(e) {
+
+				qnasRequest.onerror = function (e) {
 					console.log('qnasRequest error: ', e.value);
 					deferred.reject('qnasRequest error!');
 				};
-				
-				topicsRequest.onsuccess = function(e) {
+
+				topicsRequest.onsuccess = function (e) {
 					var topicsCursor = e.target.result;
-					if(typeof topicsCursor === 'undefined' || topicsCursor === null) {
+					if (typeof topicsCursor === 'undefined' || topicsCursor === null) {
 						console.log('topicsCursor conditional');
 						deferred.resolve(netData);
 					} else {
@@ -199,59 +261,59 @@
 						topicsCursor.continue();
 					}
 				};
-				
-				topicsRequest.onerror = function(e) {
+
+				topicsRequest.onerror = function (e) {
 					console.log('topicsRequest error: ', e.value);
 					deferred.reject('topicsRequest error!');
 				}
 			}
 			return deferred.promise;
 		};
-		self.InsertQNA = function(qna) {
+		self.InsertQNA = function (qna) {
 			var deferred = $q.defer();
-			
-			if(QNASDB === null) deferred.reject('Error: Database unavailable.');
+
+			if (QNASDB === null) deferred.reject('Error: Database unavailable.');
 			else {
 				// IDBTransaction (handlers: oncomplete, onabort, onerror)
-				var transaction = QNASDB.transaction(['qnas'], 'readwrite'); 
+				var transaction = QNASDB.transaction(['qnas'], 'readwrite');
 				var store = transaction.objectStore('qnas');
 				var addRequest = store.add(qna); // returns IDBRequest (handlers: onsuccess, onerror)
 				
-				addRequest.onsuccess = function(e) {
+				addRequest.onsuccess = function (e) {
 					deferred.resolve(e);
 				}
-				addRequest.onerror = function(e) {
+				addRequest.onerror = function (e) {
 					deferred.reject(e);
 				}
 			}
 			return deferred.promise;
 		};
-		self.UpdateQNA = function(key, qna) {
+		self.UpdateQNA = function (key, qna) {
 			var deferred = $q.defer();
-			
-			if(QNASDB === null) deferred.reject('Error: Database unavailable.');
+
+			if (QNASDB === null) deferred.reject('Error: Database unavailable.');
 			else {
 				var transaction = QNASDB.transaction(['qnas'], 'readwrite');
 				var store = transaction.objectStore('qnas');
 				var getRequest = store.get(key);
-				
-				getRequest.onsuccess = function(e) {
+
+				getRequest.onsuccess = function (e) {
 					var updateTarget = e.target.result;
-					if(typeof updateTarget !== 'undefined') {
-						
+					if (typeof updateTarget !== 'undefined') {
+
 					}
 				}
 			}
 		}
-		
+
 		return self;
 	}]);
 
-	app.directive('navMenu', function() {
+	app.directive('navMenu', function () {
 		return {
 			restrict: 'E',
 			templateUrl: 'components/Menu.html',
-			controller: function($scope) {
+			controller: function ($scope) {
 				$scope.topics = {
 					'All Topics': {
 						'Engineering': {
@@ -296,62 +358,52 @@
 						}
 					}
 				};
-				$scope.topicMenuBuilder = function(obj) {
+				$scope.topicMenuBuilder = function (obj) {
 					var str = '';
-					for(var prop in obj) {
-						if(typeof obj[prop] === 'string')
+					for (var prop in obj) {
+						if (typeof obj[prop] === 'string')
 							str += '<li><a style="cursor:pointer">' + obj[prop] + '</a></li>';
-						else if(typeof obj[prop] === 'object')
-							str += '<li class="dropdown-submenu"><a style="cursor:pointer">' + prop + '</a><ul class="dropdown-menu">' + 
-								$scope.topicMenuBuilder(obj[prop]) + '</ul></li>';
+						else if (typeof obj[prop] === 'object')
+							str += '<li class="dropdown-submenu"><a style="cursor:pointer">' + prop + '</a><ul class="dropdown-menu">' +
+							$scope.topicMenuBuilder(obj[prop]) + '</ul></li>';
 						else
 							throw ': Invalid property type';
-					}					return str;
+					} return str;
 				}
 				$scope.selectedTopic = 'All Topics';
-				
-				var topicsDropdown = '<ul class="dropdown-menu" role="menu">' + 
+
+				var topicsDropdown = '<ul class="dropdown-menu" role="menu">' +
 					$scope.topicMenuBuilder($scope.topics) + '</ul>';
 				$(topicsDropdown).insertAfter('.navbar-left li.dropdown a');
 
-				$('ul.dropdown-menu li.dropdown-submenu a').click(function() {
+				$('ul.dropdown-menu li.dropdown-submenu a').click(function () {
 					console.log($(this).text());
-					$('a.dropdown-toggle').html($(this).text().trim() + 
+					$('a.dropdown-toggle').html($(this).text().trim() +
 						' <span class="caret"></span>').data('topic', $(this).text());
 				});
 			}
 		};
 	});
 
-	app.controller('mainController', function($scope, $route, qnasDBFactory) {
-		$scope.textAreaSetup = function($element) {
-			$element.attr('ui-codemirror', '');
-		};
-		
-		$scope.codemirrorEditorOptions = {
-			lineWrapping : true,
-			lineNumbers : true,
-			readOnly: 'cursor',
-			mode: 'xml'	
-		};
-		
+	app.controller('mainController', function ($scope, $route, qnasDBFactory) {
+
 		$scope.$route = $route;
-		
+
 		$scope.netData;
-		
+
 		$scope.updateMode = false;
 		
 		// <indexedDB-interface-code>
-		$scope.getData = function() {
-			qnasDBFactory.SelectAll().then(function(result) {
+		$scope.getData = function () {
+			qnasDBFactory.SelectAll().then(function (result) {
 				$scope.netData = result;
 				window.$netData = result;
-			}, function(error) {
+			}, function (error) {
 				console.log(error);
 			});
 		};
-		
-		$scope.addQNA = function(qnaMdlVal) {
+
+		$scope.addQNA = function (qnaMdlVal) {
 			var date = new Date();
 			var qna = {
 				content: qnaMdlVal,
@@ -360,17 +412,17 @@
 				topic: null,
 				source: null
 			};
-			
-			qnasDBFactory.InsertQNA(qna).then(function(result) {
+
+			qnasDBFactory.InsertQNA(qna).then(function (result) {
 				window.ress = result;
 				console.log(result);
-			}, function(error) {
+			}, function (error) {
 				console.log(error);
 			});
 		};
-		
+
 		function init() {
-			qnasDBFactory.OpenDB().then(function() {
+			qnasDBFactory.OpenDB().then(function () {
 				//refresh
 				$scope.getData();
 			});
@@ -382,34 +434,34 @@
 		init();
 	});
 
-	app.directive('qnas', function() {
+	app.directive('qnas', function () {
 		return {
 			restrict: 'E',
 			templateUrl: 'components/QNAs.html',
-			link: function($scope, $elem, attrs) {
-				
+			link: function ($scope, $elem, attrs) {
+
 			},
-			controller: function($scope) {
+			controller: function ($scope) {
 				$scope.loc = $scope.$route;
-				$scope.$watch('loc', function(newVal, oldVal) {
+				$scope.$watch('loc', function (newVal, oldVal) {
 					console.log(newVal, oldVal);
 				});
 				$scope.qnaSet = ['a', 'b', 'c'];
 				$scope.selectedQNA = {
 					qna: null,
-					prev: function() {
-						
+					prev: function () {
+
 					},
-					next: function() {
-						
+					next: function () {
+
 					},
-					reset: function() {
-						
+					reset: function () {
+
 					}
 				};
-				
-				$scope.changeQNA = function(dir) {
-					switch(dir) {
+
+				$scope.changeQNA = function (dir) {
+					switch (dir) {
 						case 'up':
 							$scope.selectedQNA.prev();
 							break;
@@ -425,35 +477,34 @@
 							break;
 					}
 				}
-				
-				$scope.ResponseResult = { 
+
+				$scope.ResponseResult = {
 					unanswered: {
-						icon:'record',
-						panel:'default'
+						icon: 'record',
+						panel: 'default'
 					}, correct: {
-						icon:'ok-circle',
-						panel:'success'
+						icon: 'ok-circle',
+						panel: 'success'
 					}, wrong: {
-						icon:'remove-circle',
-						panel:'danger'
+						icon: 'remove-circle',
+						panel: 'danger'
 					}
 				};
-				
+
 				$scope.States = { Unanswered: 'unanswered', Correct: 'correct', Wrong: 'wrong' };
-				
-				$scope.changeState = function(qna, newState) {
+
+				$scope.changeState = function (qna, newState) {
 					qna.state = newState;
 				};
 			}
 		};
 	});
 
-	app.directive('addQna', function() {
+	app.directive('addQna', function () {
 		return {
 			restrict: 'E',
 			templateUrl: 'components/AddQNA.html',
-			link: function($scope, $elem, attrs) {
-				
+			link: function ($scope, $elem, attrs) {
 			},
 			controller: 'mainController'
 		};
